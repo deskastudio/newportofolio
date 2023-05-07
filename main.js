147b7ed45502949ae21e7d421051ac8add9865ac
deskastudio <mulyanadeska85@gmail.com>
2023-05-07 17:25:01 +0700

    kelar

---
+++ main.js
@@ -0,0 +1,19 @@
+function onFormSubmit(e) {
+    e.preventDefault() 
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("Email is Required")
+    } else if (!$(subject).val()) {
+        alert("Subject is Required")
+    } else if (!$(message).val()) {
+        alert("Message is Required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
